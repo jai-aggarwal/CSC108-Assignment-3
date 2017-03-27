2578daaf5f888d72600cbdd22bd1de0fed2e47c0
jai-aggarwal <jai.aggarwal@mail.utoronto.ca>
2017-03-27 14:28:45 -0400

    Update jai_test_file.py

--- jai_test_file.py
+++ jai_test_file.py
@@ -1,10 +1,13 @@
 data_file = open("data.txt", "r")
 good_read = data_file.readlines()
 def list_clean_up(lst):
+#This function strips the "\n" character from each element in the list.
     for i in lst:
         lst[lst.index(i)] = i.strip()
     return lst
 def data_preprocess(data_file):
+    #This function takes the cleaned-up list from the former function and sorts it into sublists, which start with username and end with 
+    #the followers the person has. The keyword "END" has been stripped from each sub-list.
     list_clean_up(data_file)
     new_list=[]
     while data_file != []:
